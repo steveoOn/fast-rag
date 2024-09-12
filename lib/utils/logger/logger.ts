2223aaa6926c6549f5c3f21c0460f768b5016d4b
steveoon <rsw_mmo@hotmail.com>
2024-09-12 19:26:53 +0800

    fix: :bug: logger bug
    部署生产后无法查看错误日志

--- lib/utils/logger/logger.ts
+++ lib/utils/logger/logger.ts
@@ -1,16 +1,48 @@
 import pino from 'pino';
 
-export const logger = pino({
-  level: process.env.NODE_ENV === 'production' ? 'info' : 'debug',
-  transport: {
-    target: 'pino-pretty',
-    options: {
-      colorize: true,
-      translateTime: 'SYS:standard',
-      ignore: 'pid,hostname',
+let logger;
+
+if (process.env.VERCEL) {
+  // Vercel 生产环境配置
+  const { logflarePinoVercel } = require('pino-logflare');
+
+  const { stream, send } = logflarePinoVercel({
+    apiKey: process.env.LOGFLARE_API_KEY,
+    sourceToken: process.env.LOGFLARE_SOURCE_TOKEN,
+  });
+
+  logger = pino(
+    {
+      browser: {
+        transmit: {
+          level: 'info',
+          send: send,
+        },
+      },
+      level: 'info',
+      base: {
+        env: process.env.VERCEL_ENV,
+        revision: process.env.VERCEL_GITHUB_COMMIT_SHA,
+      },
     },
-  },
-  base: {
-    env: process.env.NODE_ENV,
-  },
-});
+    stream
+  );
+} else {
+  // 本地开发环境配置
+  logger = pino({
+    level: process.env.NODE_ENV === 'production' ? 'info' : 'debug',
+    transport: {
+      target: 'pino-pretty',
+      options: {
+        colorize: true,
+        translateTime: 'SYS:standard',
+        ignore: 'pid,hostname',
+      },
+    },
+    base: {
+      env: process.env.NODE_ENV,
+    },
+  });
+}
+
+export { logger };
